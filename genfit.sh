7cb9eae9d69f08cad57dc83961ec113d126ed59b
juan leite <juanbsleite@live.com>
2018-11-02 17:17:24 +0000

    test

--- genfit.sh
+++ genfit.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-for i in $(seq 0 100); do
+for i in $(seq 0 5); do
 	./D2PPP -i $i gen
 done
 
-for i in $(seq 0 100); do
+for i in $(seq 0 5); do
 	./D2PPP -i $i fit	
 done
 
-./D2PPP gfplot -n 100
+./D2PPP gfplot -n 5
